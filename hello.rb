ad9185c1c322fe554f5f60aebffc01740475b833
Shajedul Islam Shuvo <helloitsshuvo@gmail.com>
2021-02-17 16:42:34 +0600

    add else statement

--- hello.rb
+++ hello.rb
@@ -5,5 +5,7 @@
 puts "What is your favorite color?"
 color = gets.chop
 if color == "blue"
-puts "Wow! My favorite color is #{color} also!"
+  puts "Wow! My favorite color is #{color} also!"
+else
+  puts "I'm not a big fan of #{color}."
 end
